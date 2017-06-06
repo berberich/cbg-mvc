f8775f3e7564a7113b3f8f5cf1f9aea2c5338580
Jason Berberich <jberberich@itscustoms.com>
2017-06-06 11:18:45 -0500

    Updated Comic Book model

--- src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -13,5 +13,6 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+        public string DisplayText => SeriesTitle + " #" + IssueNumber;
     }
 }
