9078e08e82e21ea9920a170389d451b19baa2d25
Jason Berberich <jberberich@itscustoms.com>
2017-06-06 11:49:02 -0500

    Updated Comic Book Detail view to be strongly typed

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
 {
@@ -10,18 +11,22 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "Final issue!";
-            ViewBag.Artists = new string[] {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-                };
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "Final issue!",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script"  },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"  },
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks"  },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors"  },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"  }
+                }
 
-            return View();
+            };
+            return View(comicBook);
         }
     }
 }
